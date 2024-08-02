afaaf7072f8b255b0e8eb8eb6538c9f246e82d2b
Documenter.jl <documenter@juliadocs.github.io>
2024-08-02 18:46:36 +0000

    build based on 6c5dee1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExperimentsPseudospectra","category":"page"},{"location":"#ExperimentsPseudospectra","page":"Home","title":"ExperimentsPseudospectra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExperimentsPseudospectra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExperimentsPseudospectra]","category":"page"}]
+}
