2253986717b709efab9cf50f6e11bc416eaade0f
Isaia Nisoli <nisoli@im.ufrj.br>
2025-06-20 23:06:11 +0000

    changed center radius Arnold

--- scripts/Arnold/run_Arnold_center.jl
+++ scripts/Arnold/run_Arnold_center.jl
@@ -24,7 +24,7 @@ nprocs = length(procs)
 @everywhere D = JLD2.load("../../ArnoldMatrixSchur128.jld2")
 
 λ = 0.0
-R = 0.18
+R = 0.12
 
 const filename = "./logs/log_$(location)_Arnold_$(λ)_$(R)_$datetime"
 
